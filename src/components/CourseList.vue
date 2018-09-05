b5edc56a6ceb8c634e4dd307edb6c701e035f2d1
Ehsan Aliakbar <e.aliakbar97@gmail.com>
2018-09-06 01:34:55 +0430

    Switch Course list to Vuejs

---
+++ src/components/CourseList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="course-list">
+        <div class="searchbox">
+            <input type="text" class="searchinput filter" placeholder="جستجو..."/>
+        </div>
+        <div class="list">
+            <ul id="course-group-list" v-bind:style="{height: listHeight }">
+
+            </ul>
+        </div>
+        <div class="bottom"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CourseList",
+        computed: {
+            listHeight: function() {
+                return Math.min($(window).height() - 255, $(".grid").height() - 125)
+                // return 200 + 'px'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
